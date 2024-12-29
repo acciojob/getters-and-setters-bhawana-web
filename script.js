f81bf0922998200824e4738193ebae5822591b03
bhawana-web <bhawnav161102@gmail.com>
2024-12-30 00:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //complete this code
 class Person {
   constructor(name, age) {
-    this._name = name;  // Private variable to store name
-    this._age = age;    // Private variable to store age
+    this._name = name;
+    this._age = age;
   }
 
   // Getter for name
@@ -15,33 +15,37 @@ class Person {
     this._age = age;
   }
 
-  // Getter for age (optional, to retrieve age if needed)
+  // Getter for age
   get age() {
     return this._age;
   }
 }
 
-// Student class extends Person
+// Student subclass that extends Person
 class Student extends Person {
   constructor(name, age) {
-    super(name, age);  // Call the Person constructor
+    super(name, age);
   }
 
   // Method to log that the student is studying
   study() {
-    console.log(`${this.name} is studying`);
+    console.log(${this.name} is studying);
   }
 }
 
-// Teacher class extends Person
+// Teacher subclass that extends Person
 class Teacher extends Person {
   constructor(name, age) {
-    super(name, age);  // Call the Person constructor
+    super(name, age);
   }
 
   // Method to log that the teacher is teaching
   teach() {
-    console.log(`${this.name} is teaching`);
+    console.log(${this.name} is teaching);
   }
 }
 
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
